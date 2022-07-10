b0fe048493cfd2a1cd6e77f15795a967ef2e8e29
Ju1-js <40339350+Ju1-js@users.noreply.github.com>
2022-07-09 23:00:52 -0700

    Final Commit

--- Dynamic/js/LivelyProperties.js
+++ Dynamic/js/LivelyProperties.js
@@ -1,5 +1,6 @@
 const player = $("#player")[0];
 const controls = $("#controls")[0];
+const Closecontainer = $("#closecontainer")[0];
 // const debug = $("#debug")[0];
 
 /* function log(message) {
@@ -42,11 +43,15 @@ function livelyPropertyListener(name, val) {
       switch (val) {
         case 0:
           controls.classList.add("left");
+          Closecontainer.classList.add("L");
           controls.classList.remove("right");
+          Closecontainer.classList.remove("R");
           break;
         case 1:
           controls.classList.add("right");
+          Closecontainer.classList.add("R");
           controls.classList.remove("left");
+          Closecontainer.classList.remove("L");
           break;
 
         default:
@@ -56,6 +61,7 @@ function livelyPropertyListener(name, val) {
       break;
     case "controls-visible":
       controls.classList.toggle("hide", !val);
+      Closecontainer.classList.toggle("hide", !val);
       // log(`Visible: ${val} | ${controls.classList}`);
       break;
 
