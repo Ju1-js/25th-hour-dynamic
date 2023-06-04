f2c4141f6c366837b2c6195972c6cf0c99cee22e
42willow <70784752+42Willow@users.noreply.github.com>
2023-06-04 21:46:13 +1000

    performance for visualiser

--- Dynamic/js/visualiser.js
+++ Dynamic/js/visualiser.js
@@ -17,84 +17,84 @@ let verticalScale = 8;
 let sortSoundArray = null;
 
 function calcDotSize() {
-  dotSize = dotScale * (canvas.width / numDots);
+    dotSize = dotScale * (canvas.width / numDots);
 }
 
 function setCanvasSize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  max_height = window.innerHeight * 0.5;
-  startPos = 0;
-  vizWidth = window.innerWidth;
-  midY = canvas.height - canvas.height * 0.5;
-  calcDotSize();
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    max_height = window.innerHeight * 0.5;
+    startPos = 0;
+    vizWidth = window.innerWidth;
+    midY = canvas.height - canvas.height * 0.5;
+    calcDotSize();
 }
 
 window.onload = setCanvasSize;
 window.onresize = setCanvasSize;
 
 function concentratedSort(audioArray) {
-  audioArray.sort().splice(0, audioArray.length * 0.75);
-  audioArray.push(...[...audioArray].reverse());
-  return audioArray;
+    audioArray.sort().splice(0, audioArray.length * 0.75);
+    audioArray.push(...[...audioArray].reverse());
+    return audioArray;
 }
 
 function centeredSort(audioArray) {
-  const newHalf = audioArray.splice(0, audioArray.length * 0.5);
-  audioArray.reverse().push(...newHalf);
-  return audioArray;
+    const newHalf = audioArray.splice(0, audioArray.length * 0.5);
+    audioArray.reverse().push(...newHalf);
+    return audioArray;
 }
 
 function renderAnimation(audioArray) {
-  ctx.fillStyle = foregroundColor;
+    ctx.fillStyle = foregroundColor;
 
-  for (let x = 0; x < audioArray.length; x++) {
-    const soundVal = audioArray[x];
-
-    if (rainbow) ctx.fillStyle = rainbowColors[x % rainbowColors.length] || foregroundColor;
+    for (let x = 0; x < audioArray.length; x++) {
+        const soundVal = audioArray[x];
+        if (rainbow) ctx.fillStyle = rainbowColors[x % rainbowColors.length] || foregroundColor;
 
     for (let y = 0; y < soundVal * verticalScale; y++) {
-      const vertIndex = soundVal - y / verticalScale;
-      const xPos = (x / audioArray.length) * canvas.width;
-      const yPos = y * dotSize * 2;
-      const arcRadius = Math.min(vertIndex * dotSize, dotSize);
+        const vertIndex = soundVal - y / verticalScale;
+        const xPos = (x / audioArray.length) * canvas.width;
+        const yPos = y * dotSize * 2;
+        const arcRadius = Math.min(vertIndex * dotSize, dotSize);
 
-      ctx.beginPath();
-      ctx.arc(xPos, midY - yPos, arcRadius, 0, TAU);
-      ctx.fill();
+        ctx.beginPath();
+        ctx.arc(xPos, midY - yPos, arcRadius, 0, TAU);
+        ctx.fill();
 
-      ctx.beginPath();
-      ctx.arc(xPos, midY + yPos, arcRadius, 0, TAU);
-      ctx.fill();
+        ctx.beginPath();
+        ctx.arc(xPos, midY + yPos, arcRadius, 0, TAU);
+        ctx.fill();
     }
-  }
+    }
 }
 
 function livelyAudioListener(audioArray) {
-  if (sortSoundArray) audioArray = sortSoundArray?.(audioArray);
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  renderAnimation(audioArray);
+    // If visualiser is hidden, don't bother rendering
+    if (visualiser.classList.contains("hide")) return;
+    if (sortSoundArray) audioArray = sortSoundArray?.(audioArray);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    renderAnimation(audioArray);
 }
 
 let defaultAlbumArt = "img/record.gif";
 document.getElementById("albumart").src = defaultAlbumArt;
 
+
 function livelyCurrentTrack(data) {
-  let obj = JSON.parse(data);
-  if (obj != null) {
-    document.querySelector("h2").innerText = obj.AlbumArtist;
-    document.querySelector("h2").innerText = obj.Artist;
-    document.querySelector("h1").innerText = obj.Title;
-    if (obj.Thumbnail != null) {
-      document.getElementById("albumart").src = "data:image/png;base64, " + obj.Thumbnail;
+    let obj = JSON.parse(data);
+    if (obj != null) {
+        document.querySelector("h2").innerText = obj.AlbumArtist;
+        document.querySelector("h2").innerText = obj.Artist;
+        document.querySelector("h1").innerText = obj.Title;
+        if (obj.Thumbnail != null) {
+            document.getElementById("albumart").src = "data:image/png;base64, " + obj.Thumbnail;
+        } else {
+            document.getElementById("albumart").src = defaultAlbumArt;
+        }
     } else {
-      document.getElementById("albumart").src = defaultAlbumArt;
+        document.querySelector("h2").innerText = "";
+        document.querySelector("h1").innerText = "Waiting for media...";
+        document.getElementById("albumart").src = defaultAlbumArt;
     }
-  } else {
-    document.querySelector("h2").innerText = "";
-    document.querySelector("h1").innerText = "Waiting for media...";
-    document.getElementById("albumart").src = defaultAlbumArt;
-  }
 }
