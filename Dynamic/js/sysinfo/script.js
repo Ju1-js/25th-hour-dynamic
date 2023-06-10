6472d8b1a70e24f64b75985a15d04e6e000626c3
Ju1-js <40339350+Ju1-js@users.noreply.github.com>
2023-06-09 19:12:08 -0700

    Run prettier and reformat functions

--- Dynamic/js/sysinfo/script.js
+++ Dynamic/js/sysinfo/script.js
@@ -6,16 +6,16 @@ var isChartInit = false;
 var animationDuration = 20000;
 
 var chartColors = {
-    red: 'rgb(255, 99, 132)',
-    orange: 'rgb(255, 159, 64)',
-    yellow: 'rgb(255, 205, 86)',
-    green: 'rgb(0, 192, 0)',
-    blue: 'rgb(54, 162, 235)',
-    purple: 'rgb(153, 102, 255)',
-    grey: 'rgb(201, 203, 207)',
-    lightGrey: 'rgb(105, 105, 105)',
-    black: 'rgb(0, 0, 0)', 
-    white: 'rgb(255, 255, 255)'
+  red: "rgb(255, 99, 132)",
+  orange: "rgb(255, 159, 64)",
+  yellow: "rgb(255, 205, 86)",
+  green: "rgb(0, 192, 0)",
+  blue: "rgb(54, 162, 235)",
+  purple: "rgb(153, 102, 255)",
+  grey: "rgb(201, 203, 207)",
+  lightGrey: "rgb(105, 105, 105)",
+  black: "rgb(0, 0, 0)",
+  white: "rgb(255, 255, 255)",
 };
 var color = Chart.helpers.color;
 
@@ -26,169 +26,182 @@ Chart.defaults.global.elements.pointRadius = 0;
 Chart.defaults.global.tooltips.enabled = false;
 
 var cpuChartConfig = {
-    type: 'line',
-    data: {
-        datasets: [{
-            label: 'Dataset 1',
-            backgroundColor: color(chartColors.white).alpha(1).rgbString(),
-            borderColor: chartColors.white,
-            fill: false,
-            lineTension: 0,
-            borderDash: [0, 0],
-            pointRadius: 0,
-            data: []
-        }],
+  type: "line",
+  data: {
+    datasets: [
+      {
+        label: "Dataset 1 (linear interpolation)",
+        backgroundColor: color(chartColors.white).alpha(1).rgbString(),
+        borderColor: chartColors.white,
+        fill: false,
+        lineTension: 0,
+        borderDash: [0, 0],
+        pointRadius: 0,
+        data: [],
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    legend: {
+      display: false,
     },
-    options: {   
-        maintainAspectRatio: false,   
-        legend: {
+    title: {
+      display: true,
+      text: "Processor",
+      fontColor: "#fff",
+    },
+    scales: {
+      xAxes: [
+        {
+          type: "realtime",
+          realtime: {
+            duration: animationDuration,
+            refresh: 1000,
+            delay: 1000,
+            onRefresh: onRefresh,
+          },
+          ticks: {
             display: false,
+          },
         },
-        title: {
-            display: true,
-            text: 'Processor',
-            fontColor: "#fff"
+      ],
+      yAxes: [
+        {
+          scaleLabel: {
+            display: false,
+            labelString: "%",
+          },
+          gridLines: { color: "#676062" },
+          ticks: {
+            beginAtZero: true,
+            max: 100,
+            fontColor: "#fff",
+          },
         },
-        scales: {
-            xAxes: [{
-                type: 'realtime',
-                realtime: {
-                    duration: animationDuration,
-                    refresh: 1000,
-                    delay: 1000,
-                    onRefresh: onRefresh
-                },
-                ticks: {
-                    display: false,
-                },
-            }],
-            yAxes: [{
-                scaleLabel: {
-                    display: false,
-                    labelString: '%'
-                },
-                gridLines: { color: "#676062" },
-                ticks: {
-                    beginAtZero: true,
-                    max : 100,
-                    fontColor: "#fff"
-                }
-            }]
+      ],
+    },
+  },
+};
         },
     }
 };
 
 var gpuChartConfig = {
-    type: 'line',
-    data: {
-        datasets: [{
-            label: 'Dataset 1 (linear interpolation)',
-            backgroundColor: color(chartColors.white).alpha(1).rgbString(),
-            borderColor: chartColors.white,
-            fill: false,
-            lineTension: 0,
-            borderDash: [0, 0],
-            pointRadius: 0,
-            data: []
-        }]
+  type: "line",
+  data: {
+    datasets: [
+      {
+        label: "Dataset 1 (linear interpolation)",
+        backgroundColor: color(chartColors.white).alpha(1).rgbString(),
+        borderColor: chartColors.white,
+        fill: false,
+        lineTension: 0,
+        borderDash: [0, 0],
+        pointRadius: 0,
+        data: [],
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    legend: {
+      display: false,
     },
-    options: {   
-        maintainAspectRatio: false,          
-        legend: {
+    title: {
+      display: true,
+      text: "Graphics",
+      fontColor: "#fff",
+    },
+    scales: {
+      xAxes: [
+        {
+          type: "realtime",
+          realtime: {
+            duration: animationDuration,
+            refresh: 1000,
+            delay: 1000,
+            onRefresh: onRefresh,
+          },
+          ticks: {
             display: false,
+          },
         },
-        title: {
-            display: true,
-            text: 'Graphics',
-            fontColor: "#fff"
+      ],
+      yAxes: [
+        {
+          scaleLabel: {
+            display: false,
+            labelString: "%",
+          },
+          gridLines: { color: "#676062" },
+          ticks: {
+            beginAtZero: true,
+            max: 100,
+            fontColor: "#fff",
+          },
         },
-        scales: {
-            xAxes: [{
-                type: 'realtime',
-                realtime: {
-                    duration: animationDuration,
-                    refresh: 1000,
-                    delay: 1000,
-                    onRefresh: onRefresh
-                },
-                ticks: {
-                    display: false, 
-                },
-            }],
-            yAxes: [{
-                scaleLabel: {
-                    display: false,
-                    labelString: '%'
-                },
-                gridLines: { color: "#676062" },
-                 ticks: {
-                    beginAtZero: true,
-                    max : 100,
-                    fontColor: "#fff"
-            }
-            }]
-        },
-    }
+      ],
+    },
+  },
 };
 
-function onRefresh(chart) 
-{
-    var data = [];
-    switch(chart)
-    {
-        case cpuChart:
-            data[0] = cpuCounter;
-            break;
-        case gpuChart:
-            data[0] = gpuCounter;
-            break;
-    }
+function onRefresh(chart) {
+  var data = [];
+  switch (chart) {
+    case cpuChart:
+      data[0] = cpuCounter;
+      break;
+    case gpuChart:
+      data[0] = gpuCounter;
+      break;
+  }
 
-    var i = 0;
-    chart.config.data.datasets.forEach(
-        function(dataset) {
-        dataset.data.push({
-            x: Date.now(),
-            y: data[i],
-        });
-        i++;
+  var i = 0;
+  chart.config.data.datasets.forEach(function (dataset) {
+    dataset.data.push({
+      x: Date.now(),
+      y: data[i],
     });
+    i++;
+  });
 }
 
-var cpuChart, gpuChart;
-function initChart() 
-{
-    cpuChartConfig.options.title.text = cpuName;
-    gpuChartConfig.options.title.text = gpuName;
+var cpuChart, ramChart, gpuChart;
+function initChart() {
+  cpuChartConfig.options.title.text = cpuName;
+  gpuChartConfig.options.title.text = gpuName;
 
-    var ctxCpu = document.getElementById('cpuChart').getContext('2d');
-    cpuChart = new Chart(ctxCpu, cpuChartConfig);
+  var ctxCpu = document.getElementById("cpuChart").getContext("2d");
+  cpuChart = new Chart(ctxCpu, cpuChartConfig);
 
-    var ctxGpu = document.getElementById('gpuChart').getContext('2d');
-    gpuChart = new Chart(ctxGpu, gpuChartConfig);
-};
+
+  var ctxGpu = document.getElementById("gpuChart").getContext("2d");
+  gpuChart = new Chart(ctxGpu, gpuChartConfig);
+}
 
 function livelySystemInformation(data) {
-    if (!sysinfo.classList.contains("hide")) {
-        gpuChartConfig.options.scales.xAxes[0].realtime.duration = animationDuration;
-        cpuChartConfig.options.scales.xAxes[0].realtime.duration = animationDuration;
-
-        var obj = JSON.parse(data);
-        cpuName = "CPU (% usage)";
-        gpuName = "GPU (% usage)";
-
-        cpuCounter = obj.CurrentCpu;
-        gpuCounter = obj.CurrentGpu3D;
-        if(!isChartInit)
-        {
-            isChartInit = true;
-            initChart();
-        }
-    } else {
-        gpuChartConfig.options.scales.xAxes[0].realtime.duration = 0;
-        cpuChartConfig.options.scales.xAxes[0].realtime.duration = 0;
+  if (!sysinfo.classList.contains("hide")) {
+    var obj = JSON.parse(data);
+    if (!sysinfo.children[0].classList.contains("hide")) {
+      cpuName = "CPU (% usage)";
+      cpuChartConfig.options.scales.xAxes[0].realtime.duration =
+        animationDuration;
+      cpuCounter = obj.CurrentCpu;
     }
-}
+    if (!sysinfo.children[2].classList.contains("hide")) {
+      gpuName = "GPU (% usage)";
+      gpuChartConfig.options.scales.xAxes[0].realtime.duration =
+        animationDuration;
+      gpuCounter = obj.CurrentGpu3D;
+    }
 
-//test
-//initChart();
+    if (!isChartInit) {
+      isChartInit = true;
+      initChart();
+    }
+  } else {
+    cpuChartConfig.options.scales.xAxes[0].realtime.duration = 0;
+    gpuChartConfig.options.scales.xAxes[0].realtime.duration = 0;
+  }
+}
